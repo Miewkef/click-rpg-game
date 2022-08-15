36ce4468a947d8fed5a3d4c848fb97b9f0b16699
Miewkef <86587918+Miewkef@users.noreply.github.com>
2022-08-15 18:12:38 +0600

    v0.3

--- gameCode.js
+++ gameCode.js
@@ -1,31 +1,17 @@
 var showMoney = document.getElementsByClassName("Money")[0].style.visibility = "hidden";
-var openInv = document.getElementById('openInv');
 
-
-
-
-function incrementButton(){
-    let element = document.getElementById('incrementStraw');
-    let value = element.innerHTML;
-
-    ++value;
-
-    console.log(value);
-    document.getElementById('incrementStraw').innerHTML = value;
-    if (value == 10){
-        alert('Hooray! Now u can sell your strawberries')
+let sbCount = 0;
+let rbCount = 0;
+let grpCount = 0;
+let bbCount = 0;
+document.getElementById('berry').onclick = function(){
+    sbCount +=1;
+    document.getElementById('incrementStraw').innerHTML=sbCount;
+    if (sbCount!=0){
         showMoney = document.getElementsByClassName("Money")[0].style.visibility = "visible";
     }
 }
 
-function showInv(){
-    let element = document.getElementById('incrementStraw');
-    let valueStraw = element.innerHTML;
-    let element1 = document.getElementById('incrementRasp');
-    let valueRasp =element1.innerHTML;
-    let element2 = document.getElementById('incrementGrape');
-    let valueGrape =element2.innerHTML;
-    let element3 = document.getElementById('incrementBlack');
-    let valueBlack =element3.innerHTML; 
-    alert(`you have:\n${valueStraw}sb\n${valueRasp}rb\n${valueGrape}grp\n${valueBlack}bb`)
+document.getElementById('inventory').onclick = function(){
+    (sbCount!=0) ? alert(`you have\n${sbCount} sb\n${rbCount} rb\n${grpCount} grp\n${bbCount} bb`) : alert('you have nothing to lose');
 }
